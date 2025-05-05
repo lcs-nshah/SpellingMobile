4594b457432087f6673c330d4bd1bb6420ea2f31
Neil Shah <nshah@lcs.on.ca>
2025-05-05 12:02:59 -0400

    Writen the basic shell of the filtering function;

---
+++ SpellingMobile/Functions/Filtering.swift
@@ -0,0 +1,24 @@
+//
+//  Filtering.swift
+//  SpellingMobile
+//
+//  Created by Neil Shah on 2025-05-05.
+//
+
+// Define the function
+//
+// name: filtering
+//
+// inputs (parameters):
+//          the original array (originalList)
+//          what to filter on (desiredOutcome)
+//
+//          output (return type): an array filtered based on outcome
+//
+func filtering(
+    originalList: [Result],
+    on desiredOutcome: Outcome
+) -> [Outcome] {
+    // Return an empty array
+    return []
+}
